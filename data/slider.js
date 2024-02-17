25514dbcec89e2de847f49f754cf206ffd6b1d8b
zamwam <159726433+zamwam@users.noreply.github.com>
2024-02-17 17:16:52 -0500

    1.0T

--- data/slider.js
+++
@@ -1,9 +0,0 @@
-//let slides = document.querySelectorAll('.slide');
-//let currentSlide = 0;
-//let slideInterval = setInterval(nextSlide, 3000);
-//
-//function nextSlide() {
-//  slides[currentSlide].className = 'slide';
-//  currentSlide = (currentSlide + 1) % slides.length;
-//  slides[currentSlide].className = 'slide active';
-//}
